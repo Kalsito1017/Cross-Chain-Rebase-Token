e0cb19616dfb673de205d6eaf1f019e9418f7a4c
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-07-27 20:31:55 +0300

    Compiler/Libraries Errors
    Had errors for compiling and added function configureTokenPool and fixed libraries paths

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -10,76 +10,164 @@ import {IERC20} from "lib/ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IAccessControl} from "lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
 import {RebaseTokenPool} from "src/RebaseTokenPool.sol";
-import {CCIPLocalSimulatorFork, Register} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {CCIPLocalSimulatorFork} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 import {RegistryModuleOwnerCustom} from "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
 import {TokenAdminRegistry} from "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
+import {TokenPool} from "lib/ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {RateLimiter} from "lib/ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
 
 contract CrossChainTest is Test {
-    address constant owner = makeAddr("owner");
+    address public owner = makeAddr("owner");
+    address alice = makeAddr("alice");
+    CCIPLocalSimulatorFork public ccipLocalSimulatorFork;
+    uint256 public SEND_VALUE = 1e5;
+
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
-    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
-    RebaseToken sepoliaToken;
-    RebaseToken arbSepoliaToken;
-    Vault vault;
+
+    RebaseToken destRebaseToken;
+    RebaseToken sourceRebaseToken;
+
+    RebaseTokenPool destPool;
+    RebaseTokenPool sourcePool;
+
+    TokenAdminRegistry tokenAdminRegistrySepolia;
+    TokenAdminRegistry tokenAdminRegistryarbSepolia;
+
     Register.NetworkDetails sepoliaNetworkDetails;
     Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    RegistryModuleOwnerCustom registryModuleOwnerCustomSepolia;
+    RegistryModuleOwnerCustom registryModuleOwnerCustomarbSepolia;
+
+    Vault vault;
     /// @notice Sets up the test environment with multiple forks and a persistent local CCIP simulator
     /// @dev Initializes Sepolia and Arbitrum Sepolia forks, and deploys a persistent CCIPLocalSimulatorFork instance
     function setUp() public {
-        // Create and select the Sepolia fork as the active fork
-        sepoliaFork = vm.createSelectFork("sepolia");
+        address[] memory allowlist = new address[](0);
 
-        // Create an Arbitrum Sepolia fork (not selected by default)
-        arbSepoliaFork = vm.createFork("arb-sepolia");
+        // sourceDeployer = new SourceDeployer();
 
-        // Deploy the local CCIP simulator used for testing CCIP behavior without real networks
+        // 1. Setup the Sepolia and arb forks
+        sepoliaFork = vm.createSelectFork("eth");
+        arbSepoliaFork = vm.createFork("arb");
+
+        //NOTE: what does this do?
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
-
-        // Make the simulator persistent so its state is retained across test functions
         vm.makePersistent(address(ccipLocalSimulatorFork));
-        //Deploy on Sepolia
+
+        // 2. Deploy and configure on the source chain: Sepolia
+        //sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        //(sourceRebaseToken, sourcePool, vault) = sourceDeployer.run(owner);
         sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
             block.chainid
         );
         vm.startPrank(owner);
-        sepoliaToken = new RebaseToken();
-        vault = new Vault(IRebaseToken(address(sepoliaToken)));
-        sepoliaPool = new RebaseTokenPool(
-            IERC20(address(sepoliaToken)),
-            new address[](0),
+        sourceRebaseToken = new RebaseToken();
+        console.log("source rebase token address");
+        console.log(address(sourceRebaseToken));
+        console.log("Deploying token pool on Sepolia");
+        sourcePool = new RebaseTokenPool(
+            IERC20(address(sourceRebaseToken)),
+            allowlist,
             sepoliaNetworkDetails.rmnProxyAddress,
             sepoliaNetworkDetails.routerAddress
         );
-        sepoliaToken.grantMintAndBurnRole(address(vault));
-        sepoliaToken.grantMintAndBurnRole(address(sepoliaPool));
-        RegistryModuleOwnerCustom(
+        // deploy the vault
+        vault = new Vault(IRebaseToken(address(sourceRebaseToken)));
+        // add rewards to the vault
+        vm.deal(address(vault), 1e18);
+        // Set pool on the token contract for permissions on Sepolia
+        sourceRebaseToken.grantMintAndBurnRole(address(sourcePool));
+        sourceRebaseToken.grantMintAndBurnRole(address(vault));
+        // Claim role on Sepolia
+        registryModuleOwnerCustomSepolia = RegistryModuleOwnerCustom(
             sepoliaNetworkDetails.registryModuleOwnerCustomAddress
-        ).registerAdminViaOwner(address(sepoliaToken));
-        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
-            .acceptAdminRole(address(sepoliaToken));
-        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
-            .setPool(address(sepoliaToken), address(sepoliaPool));
+        );
+        registryModuleOwnerCustomSepolia.registerAdminViaOwner(
+            address(sourceRebaseToken)
+        );
+        // Accept role on Sepolia
+        tokenAdminRegistrySepolia = TokenAdminRegistry(
+            sepoliaNetworkDetails.tokenAdminRegistryAddress
+        );
+        tokenAdminRegistrySepolia.acceptAdminRole(address(sourceRebaseToken));
+        // Link token to pool in the token admin registry on Sepolia
+        tokenAdminRegistrySepolia.setPool(
+            address(sourceRebaseToken),
+            address(sourcePool)
+        );
         vm.stopPrank();
-        //Deploy on Arbitrum-Sepolia
-        arbSepoliaToken = ccipLocalSimulatorFork.getNetworkDetails(
+
+        // 3. Deploy and configure on the destination chain: Arbitrum
+        // Deploy the token contract on Arbitrum
+        vm.selectFork(arbSepoliaFork);
+        vm.startPrank(owner);
+        console.log("Deploying token on Arbitrum");
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
             block.chainid
         );
-        vm.startPrank(owner);
-        arbSepoliaToken = new RebaseToken();
-        arbSepoliaPool = new RebaseTokenPool(
-            IERC20(address(arbSepoliaToken)),
-            new address[](0),
+        destRebaseToken = new RebaseToken();
+        console.log("dest rebase token address");
+        console.log(address(destRebaseToken));
+        // Deploy the token pool on Arbitrum
+        console.log("Deploying token pool on Arbitrum");
+        destPool = new RebaseTokenPool(
+            IERC20(address(destRebaseToken)),
+            allowlist,
             arbSepoliaNetworkDetails.rmnProxyAddress,
             arbSepoliaNetworkDetails.routerAddress
         );
-        RegistryModuleOwnerCustom(
+        // Set pool on the token contract for permissions on Arbitrum
+        destRebaseToken.grantMintAndBurnRole(address(destPool));
+        // Claim role on Arbitrum
+        registryModuleOwnerCustomarbSepolia = RegistryModuleOwnerCustom(
             arbSepoliaNetworkDetails.registryModuleOwnerCustomAddress
-        ).registerAdminViaOwner(address(arbSepoliaToken));
-        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress)
-            .acceptAdminRole(address(arbSepoliaToken));
-        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress)
-            .setPool(address(arbSepoliaToken), address(arbSepoliaPool));
+        );
+        registryModuleOwnerCustomarbSepolia.registerAdminViaOwner(
+            address(destRebaseToken)
+        );
+        // Accept role on Arbitrum
+        tokenAdminRegistryarbSepolia = TokenAdminRegistry(
+            arbSepoliaNetworkDetails.tokenAdminRegistryAddress
+        );
+        tokenAdminRegistryarbSepolia.acceptAdminRole(address(destRebaseToken));
+        // Link token to pool in the token admin registry on Arbitrum
+        tokenAdminRegistryarbSepolia.setPool(
+            address(destRebaseToken),
+            address(destPool)
+        );
         vm.stopPrank();
+    }
+    function configureTokenPool(
+        uint256 fork,
+        address localPool,
+        uint64 remoteChainSelector,
+        address remotePool,
+        address remoteTokenAddress
+    ) public {
+        vm.selectFork(fork);
+        vm.prank(owner);
+        bytes[] memory remotePoolAddresses = new bytes[](1);
+        remotePoolAddresses[0] = abi.encode(remotePool);
+        TokenPool.ChainUpdate[]
+            memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            remotePoolAddresses: remotePoolAddresses,
+            remoteTokenAddress: abi.encode(remoteTokenAddress),
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: false,
+                capacity: 0,
+                rate: 0
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: false,
+                capacity: 0,
+                rate: 0
+            })
+        });
+
+        TokenPool(localPool).applyChainUpdates(new uint64[](0), chainsToAdd);
     }
 }
